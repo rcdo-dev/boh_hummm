3991806c50dba6093340049d2206c71ef14d5567
rcdo-dev <rcdo.dev@gmail.com>
2025-02-19 17:05:36 -0300

    feat: implementing readAllMotorcycles method.

--- test/data/repositories/motorcycle_repository_test.dart
+++ test/data/repositories/motorcycle_repository_test.dart
@@ -1,13 +1,18 @@
-import 'package:boh_hummm/data/services/sqlite/connection_db/i_connection_db.dart';
-import 'package:boh_hummm/utils/result.dart';
+import 'package:boh_hummm/utils/extensions/result_cast.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_test/flutter_test.dart';
+
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
+
+import 'package:boh_hummm/data/services/sqlite/connection_db/i_connection_db.dart';
+import 'package:boh_hummm/domain/entities/motorcycle_entity.dart';
+import 'package:boh_hummm/utils/result.dart';
 
 import '../../../testing/data/model/motorcycle_model.dart';
 import '../../../testing/data/repositories/motorcycle_repository.dart';
 import '../../../testing/data/services/sqlite/connection_db/impl/connection_db_sqlite.dart';
 import '../../../testing/data/services/sqlite/impl/motorcycle_service.dart';
+import '../../../testing/data/services/sqlite/impl/user_service.dart';
 
 void main() {
   group('Test group for the motorcycle repository.', () {
@@ -20,13 +25,16 @@ void main() {
 
     late IConnectionDb connection;
     late MotorcycleService motorcycleService;
+    late UserService userService;
     late MotorcycleRepository motorcycleRepository;
 
     setUp(() {
       connection = ConnectionDbSqlite();
       motorcycleService = MotorcycleService(connection: connection);
+      userService = UserService(connection: connection);
       motorcycleRepository = MotorcycleRepository(
         motorcycleService: motorcycleService,
+        userService: userService,
       );
     });
 
@@ -36,6 +44,17 @@ void main() {
         idUser: 3,
       );
       expect(result, isA<Ok<void>>());
+    });
+
+    test('It should return a list of motorcycles with their respective users.',
+        () async {
+      final result = await motorcycleRepository.readAllMotorcycles();
+      if (kDebugMode) {
+        for (var moto in result.asOk.value){
+          print(moto.user?.name);
+        }
+      }
+      expect(result, isA<Ok<List<MotorcycleEntity>>>());
     });
 
     tearDownAll(() {
--- testing/data/repositories/motorcycle_repository.dart
+++ testing/data/repositories/motorcycle_repository.dart
@@ -1,15 +1,21 @@
 import 'package:boh_hummm/data/model/motorcycle_model.dart';
 import 'package:boh_hummm/domain/entities/motorcycle_entity.dart';
+import 'package:boh_hummm/domain/entities/user_entity.dart';
+import 'package:boh_hummm/utils/extensions/result_cast.dart';
 import 'package:boh_hummm/utils/result.dart';
 
 import '../services/sqlite/impl/motorcycle_service.dart';
+import '../services/sqlite/impl/user_service.dart';
 
 class MotorcycleRepository {
   final MotorcycleService _motorcycleService;
+  final UserService _userService;
 
   MotorcycleRepository({
     required MotorcycleService motorcycleService,
-  }) : _motorcycleService = motorcycleService;
+    required UserService userService,
+  })  : _motorcycleService = motorcycleService,
+        _userService = userService;
 
   Future<Result<void>> createMotorcycle({
     required MotorcycleEntity motorcycleEntity,
@@ -26,5 +32,32 @@ class MotorcycleRepository {
     } on Exception catch (e, s) {
       return Result.error(e, s);
     }
+  }
+
+  Future<Result<List<MotorcycleEntity>>> readAllMotorcycles() async {
+    var resultMotorcycles = await _motorcycleService.readAll();
+    var resultUsers = await _userService.readAll();
+
+    var userEntity = UserEntity();
+    for (var motorcycle in resultMotorcycles.asOk.value) {
+      var motUserId = motorcycle['mot_use_id'] as int;
+      userEntity = UserEntity.fromMap(
+        resultUsers.asOk.value
+            .firstWhere((element) => element['use_id'] == motUserId),
+      );
+    }
+
+    // Tem que ser uma lista de usuários
+
+    var motorcycleEntity = resultMotorcycles.asOk.value.map((element) {
+      return MotorcycleEntity(
+        brand: element['mot_brand'] as String,
+        type: element['mot_type'] as String,
+        cylinderCapacity: element['mot_cylinder_capacity'] as double,
+        user: userEntity,
+      );
+    }).toList();
+
+    return Result.ok(motorcycleEntity);
   }
 }
